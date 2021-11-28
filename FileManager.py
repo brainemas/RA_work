42c5a574edd20420c38805f88de6973415bdf6ce
Станислав Терешкин <94097856+brainemas@users.noreply.github.com>
2021-11-28 11:10:12 +0700

    Create FileManager.py
    Класс FileManager

---
+++ FileManager.py
@@ -0,0 +1,23 @@
+from Folder import Folder
+from File import File
+from Storage import Storage
+
+
+class FileManager:
+    def __init__(self):
+        self.__storage = Storage()
+
+    def get_all_files(self):
+        return self.__storage.get_all()
+
+    def add_file(self, name: File):
+        self.__storage.put_file(name)
+
+    def remove_file(self, name: str):
+        self.__storage.delete_file(name)
+
+    def add_folder(self, name: Folder):
+        self.__storage.put_folder(name)
+
+    def remove_folder(self, name: str):
+        self.__storage.delete_folder(name)
