da6d7cd7afe50a830f9fd4cb6078b7475538ead6
Станислав Терешкин <94097856+brainemas@users.noreply.github.com>
2021-12-01 18:22:11 +0700

    Update Storage.py
    Изменены функции с использованием библиотеки pathlib:
    - создание папки/файла
    - удаление папки/файла
    - получение списка файлов а папке
    - получение информации о файле

--- Storage.py
+++ Storage.py
@@ -1,6 +1,10 @@
+from datetime import datetime
 from Folder import Folder
 from File import File
+from pathlib import Path, PurePath
 from typing import Iterable
+import time
+import os
 
 
 class ReadOnlyStorage(object):
@@ -8,19 +12,32 @@ class ReadOnlyStorage(object):
         self._files = {}
         self._folders = {}
 
-    def get_all(self) -> Iterable[File]:
-        return self._files.values()
+    def get_all(self, path: str):
+        return list(map(str, sorted(Path(path).glob('*'))))
+        #sorted(os.listdir(Path.cwd() / path))
 
-    def put_file(self, file: File):
-        self._files[file.name] = file
+    def put_file(self, name: str):
+        if Path(name).exists():
+            print('Такой файл уже существует.')
+        else:
+            Path(name).touch()
+            print('Файл', name, 'добавлен.')
+            File(name, Path(name).stat().st_size, time.ctime(Path(name).stat().st_ctime), Path(name).stat().st_uid, PurePath(name).parent)
 
-    def put_folder(self, folder: Folder):
-        self._folders[folder.name] = folder
+    def put_folder(self, name: str):
+        if Path(name).exists():
+            print('Такая папка уже существует.')
+        else:
+            Path(name).mkdir()
+            print('Папка', name, 'добавлена.')
+            Folder(name, datetime.today(), Path(name).stat().st_uid)
 
 
 class Storage(ReadOnlyStorage):
     def delete_file(self, name: str):
-        del self._files[name]
+        Path(name).unlink()
+        print('Файл удален.')
 
     def delete_folder(self, name: str):
-        del self._folders[name]
+        Path(name).rmdir()
+        print('Папка удалена.')
