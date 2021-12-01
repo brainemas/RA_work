48a9bcac8ff404ae657673db4cacd986d6590d7a
Станислав Терешкин <94097856+brainemas@users.noreply.github.com>
2021-12-01 18:20:37 +0700

    Update FileManager.py
    Изменены типы

--- FileManager.py
+++ FileManager.py
@@ -7,16 +7,16 @@ class FileManager:
     def __init__(self):
         self.__storage = Storage()
 
-    def get_all_files(self):
-        return self.__storage.get_all()
+    def get_all_files(self, path: str):
+        return self.__storage.get_all(path)
 
-    def add_file(self, name: File):
+    def add_file(self, name: str):
         self.__storage.put_file(name)
 
     def remove_file(self, name: str):
         self.__storage.delete_file(name)
 
-    def add_folder(self, name: Folder):
+    def add_folder(self, name: str):
         self.__storage.put_folder(name)
 
     def remove_folder(self, name: str):
